fd63d3938538ac9b09c686f91dd0f2f206145326
M1sterZag <maximzaglubotsky@gmail.com>
2025-02-17 22:09:30 +0300

    style(tailwind): add colors, font, sizes, border radius

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                fon: "#111214",
+                primary: "#8244FF",
+                primary_hover: "#8E5FEC",
+                secondary: "#1B1C1E",
+                accent: "#50FA7B",
+                accent_hover: "#2AE359",
+                text: "#E8D8C9",
+            },
+            fontFamily: {
+                montserrat: ["Montserrat", "sans-serif"],
+            },
+            fontSize: {
+                s48: "48px",
+                s32: "32px",
+                s20: "20px",
+                s16: "16px",
+                s12: "12px"
+            },
+            borderRadius: {
+                brs: "8px",
+            },
+        },
+    },
+    plugins: [],
 };
