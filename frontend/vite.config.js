7ed196a43a63a4b81d841db8f7260b07a4731065
M1sterZag <maximzaglubotsky@gmail.com>
2025-02-19 22:14:51 +0300

    style(frontend): add styles from figma design

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'url'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      "/commands": "http://localhost:8000/"
+    }
+  }
 })
