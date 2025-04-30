389e099e6905d7bc24a43f30dc349605c637b928
M1sterZag <maximzaglubotsky@gmail.com>
2025-04-30 16:06:21 +0300

    refactor(homepage): change bonuses

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -168,13 +168,12 @@
       <div class="cards grid grid-cols-3 mx-auto gap-10 mt-[45px]">
         <div
             class="card !bg-secondary text-text p-[20px] rounded-lg border-none flex flex-col items-stretch h-full">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-               stroke="currentColor" class="w-[40px] h-[40px] text-primary">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"/>
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 28 28" class="text-primary">
+            <path fill="currentColor"
+                  d="M17.754 11c.966 0 1.75.784 1.75 1.75v6.749a5.501 5.501 0 0 1-11.002 0V12.75c0-.966.783-1.75 1.75-1.75zM3.75 11l4.382-.002a2.73 2.73 0 0 0-.621 1.532l-.01.22v6.749c0 1.133.291 2.199.8 3.127A4.5 4.5 0 0 1 2 18.499V12.75A1.75 1.75 0 0 1 3.751 11m16.124-.002L24.25 11c.966 0 1.75.784 1.75 1.75v5.75a4.5 4.5 0 0 1-6.298 4.127l.056-.102c.429-.813.69-1.729.738-2.7l.008-.326V12.75c0-.666-.237-1.276-.63-1.752M14 3a3.5 3.5 0 1 1 0 7a3.5 3.5 0 0 1 0-7m8.003 1a3 3 0 1 1 0 6a3 3 0 0 1 0-6M5.997 4a3 3 0 1 1 0 6a3 3 0 0 1 0-6"/>
           </svg>
-          <p class="text-s20 mt-4 mb-2">Отключение рекламы</p>
-          <p class="text-s16 mt-auto">Наслаждайтесь приложением без отвлекающей рекламы</p>
+          <p class="text-s20 mt-4 mb-2">Больше союзников</p>
+          <p class="text-s16 mt-auto">Создавайте команды с более чем 5 участниками</p>
         </div>
 
         <div
@@ -183,18 +182,18 @@
             <path fill="currentColor"
                   d="M3 2.25a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 0 1.5h-8.5A.75.75 0 0 1 3 2.25m5.53 2.97l3.75 3.75a.749.749 0 1 1-1.06 1.06L8.75 7.561v6.689a.75.75 0 0 1-1.5 0V7.561L4.78 10.03a.749.749 0 1 1-1.06-1.06l3.75-3.75a.75.75 0 0 1 1.06 0"/>
           </svg>
-          <p class="text-s20 mt-4 mb-2">Поднятие профиля или команды в поиске</p>
+          <p class="text-s20 mt-4 mb-2">Поднятие команды в поиске</p>
           <p class="text-s16 mt-auto">Будьте на вершине списка — вас найдут первыми!</p>
         </div>
 
         <div
             class="card !bg-secondary text-text p-[20px] rounded-lg border-none flex flex-col items-stretch h-full">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-[40px] h-[40px] text-primary" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" class="text-primary">
             <path fill="currentColor"
-                  d="M4 11q-.425 0-.712-.288T3 10V4q0-.425.288-.712T4 3h6q.425 0 .713.288T11 4v6q0 .425-.288.713T10 11zm10 0q-.425 0-.712-.288T13 10V4q0-.425.288-.712T14 3h6q.425 0 .713.288T21 4v6q0 .425-.288.713T20 11zM4 21q-.425 0-.712-.288T3 20v-6q0-.425.288-.712T4 13h6q.425 0 .713.288T11 14v6q0 .425-.288.713T10 21zm13 0q-.425 0-.712-.288T16 20v-2h-2.025q-.425 0-.7-.288T13 17t.288-.712T14 16h2v-2.025q0-.425.288-.7T17 13t.713.288T18 14v2h2.025q.425 0 .7.288T21 17t-.288.713T20 18h-2v2.025q0 .425-.288.7T17 21"/>
+                  d="M2 4a2 2 0 0 1 2-2h3v2H4v3H2zm20 4v3h-2V4h-3V2h3a2 2 0 0 1 2 2m-2 16v-3h2v3a2 2 0 0 1-2 2h-3v-2zM2 20v-3h2v3h3v2H4a2 2 0 0 1-2-2m8-18h4v2h-4zm0 18h4v2h-4zm10-10h2v4h-2zM2 10h2v4H2z"/>
           </svg>
-          <p class="text-s20 mt-4 mb-2">Кастомизация профиля</p>
-          <p class="text-s16 mt-auto">Добавьте уникальный стиль, чтобы выделиться среди других</p>
+          <p class="text-s20 mt-4 mb-2">Выделение никнейма</p>
+          <p class="text-s16 mt-auto">Получите уникальный стиль, чтобы выделиться среди других</p>
         </div>
       </div>
     </div>
@@ -209,128 +208,128 @@
     </div>
 
     <!-- Цены -->
-    <div class="prices grid grid-cols-3 gap-4 justify-center mt-[45px]">
-      <div class="card !bg-secondary rounded-lg p-[32px] border-secondary border-2 hover:border-accent flex flex-col">
-        <h1 class="text-s32 font-medium text-text">Месяц</h1>
-        <div class="flex items-baseline gap-1">
-          <p class="text-s48 font-medium text-primary">300₽</p>
-          <p class="text-s16 text-text opacity-80">/месяц</p>
-        </div>
-        <p class="text-s16 text-text mt-2">Хороший план чтобы начать пользоваться сервисом</p>
-        <ul class="text-text text-s16 mt-3 flex-grow">
-          <li class="flex items-center gap-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 1</span>
-          </li>
-          <li class="flex items-center gap-1 mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 2</span>
-          </li>
-          <li class="flex items-center gap-1 mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 3</span>
-          </li>
-        </ul>
-        <div class="mt-4 w-full">
-          <router-link to="/premium">
-            <button
-                class="bg-accent !text-secondary py-2 px-4 rounded-lg font-medium text-[20px] hover:bg-accent_hover w-full">
-              Купить
-            </button>
-          </router-link>
-        </div>
-      </div>
+    <!--    <div class="prices grid grid-cols-3 gap-4 justify-center mt-[45px]">-->
+    <!--      <div class="card !bg-secondary rounded-lg p-[32px] border-secondary border-2 hover:border-accent flex flex-col">-->
+    <!--        <h1 class="text-s32 font-medium text-text">Месяц</h1>-->
+    <!--        <div class="flex items-baseline gap-1">-->
+    <!--          <p class="text-s48 font-medium text-primary">300₽</p>-->
+    <!--          <p class="text-s16 text-text opacity-80">/месяц</p>-->
+    <!--        </div>-->
+    <!--        <p class="text-s16 text-text mt-2">Хороший план чтобы начать пользоваться сервисом</p>-->
+    <!--        <ul class="text-text text-s16 mt-3 flex-grow">-->
+    <!--          <li class="flex items-center gap-1">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 1</span>-->
+    <!--          </li>-->
+    <!--          <li class="flex items-center gap-1 mt-2">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 2</span>-->
+    <!--          </li>-->
+    <!--          <li class="flex items-center gap-1 mt-2">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 3</span>-->
+    <!--          </li>-->
+    <!--        </ul>-->
+    <!--        <div class="mt-4 w-full">-->
+    <!--          <router-link to="/premium">-->
+    <!--            <button-->
+    <!--                class="bg-accent !text-secondary py-2 px-4 rounded-lg font-medium text-[20px] hover:bg-accent_hover w-full">-->
+    <!--              Купить-->
+    <!--            </button>-->
+    <!--          </router-link>-->
+    <!--        </div>-->
+    <!--      </div>-->
 
-      <div class="card !bg-secondary rounded-lg p-[32px] border-secondary border-2 hover:border-accent flex flex-col">
-        <h1 class="text-s32 font-medium text-text">3 Месяца</h1>
-        <div class="flex items-baseline gap-1">
-          <p class="text-s48 font-medium text-primary">300₽</p>
-          <p class="text-s16 text-text opacity-80">/месяц</p>
-        </div>
-        <p class="text-s16 text-text mt-2">Идеальный выбор для тех, кто хочет оценить все возможности сервиса с
-          выгодой!</p>
-        <ul class="text-text text-s16 mt-3 flex-grow">
-          <li class="flex items-center gap-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 1</span>
-          </li>
-          <li class="flex items-center gap-1 mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 2</span>
-          </li>
-          <li class="flex items-center gap-1 mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 3</span>
-          </li>
-        </ul>
-        <div class="mt-4 w-full">
-          <router-link to="/premium">
-            <button
-                class="bg-accent !text-secondary py-2 px-4 rounded-lg font-medium text-[20px] hover:bg-accent_hover w-full">
-              Купить
-            </button>
-          </router-link>
-        </div>
-      </div>
+    <!--      <div class="card !bg-secondary rounded-lg p-[32px] border-secondary border-2 hover:border-accent flex flex-col">-->
+    <!--        <h1 class="text-s32 font-medium text-text">3 Месяца</h1>-->
+    <!--        <div class="flex items-baseline gap-1">-->
+    <!--          <p class="text-s48 font-medium text-primary">300₽</p>-->
+    <!--          <p class="text-s16 text-text opacity-80">/месяц</p>-->
+    <!--        </div>-->
+    <!--        <p class="text-s16 text-text mt-2">Идеальный выбор для тех, кто хочет оценить все возможности сервиса с-->
+    <!--          выгодой!</p>-->
+    <!--        <ul class="text-text text-s16 mt-3 flex-grow">-->
+    <!--          <li class="flex items-center gap-1">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 1</span>-->
+    <!--          </li>-->
+    <!--          <li class="flex items-center gap-1 mt-2">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 2</span>-->
+    <!--          </li>-->
+    <!--          <li class="flex items-center gap-1 mt-2">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 3</span>-->
+    <!--          </li>-->
+    <!--        </ul>-->
+    <!--        <div class="mt-4 w-full">-->
+    <!--          <router-link to="/premium">-->
+    <!--            <button-->
+    <!--                class="bg-accent !text-secondary py-2 px-4 rounded-lg font-medium text-[20px] hover:bg-accent_hover w-full">-->
+    <!--              Купить-->
+    <!--            </button>-->
+    <!--          </router-link>-->
+    <!--        </div>-->
+    <!--      </div>-->
 
-      <div class="card !bg-secondary rounded-lg p-[32px] border-secondary border-2 hover:border-accent flex flex-col">
-        <h1 class="text-s32 font-medium text-text">Год</h1>
-        <div class="flex items-baseline gap-1">
-          <p class="text-s48 font-medium text-primary">300₽</p>
-          <p class="text-s16 text-text opacity-80">/месяц</p>
-        </div>
-        <p class="text-s16 text-text mt-2">Самый выгодный план для тех, кто ценит качество и долгосрочные решения.</p>
-        <ul class="text-text text-s16 mt-3 flex-grow">
-          <li class="flex items-center gap-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 1</span>
-          </li>
-          <li class="flex items-center gap-1 mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 2</span>
-          </li>
-          <li class="flex items-center gap-1 mt-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>
-            </svg>
-            <span>Преимущество 3</span>
-          </li>
-        </ul>
-        <div class="mt-4 w-full">
-          <router-link to="/premium">
-            <button
-                class="bg-accent !text-secondary py-2 px-4 rounded-lg font-medium text-[20px] hover:bg-accent_hover w-full">
-              Купить
-            </button>
-          </router-link>
-        </div>
-      </div>
-    </div>
+    <!--      <div class="card !bg-secondary rounded-lg p-[32px] border-secondary border-2 hover:border-accent flex flex-col">-->
+    <!--        <h1 class="text-s32 font-medium text-text">Год</h1>-->
+    <!--        <div class="flex items-baseline gap-1">-->
+    <!--          <p class="text-s48 font-medium text-primary">300₽</p>-->
+    <!--          <p class="text-s16 text-text opacity-80">/месяц</p>-->
+    <!--        </div>-->
+    <!--        <p class="text-s16 text-text mt-2">Самый выгодный план для тех, кто ценит качество и долгосрочные решения.</p>-->
+    <!--        <ul class="text-text text-s16 mt-3 flex-grow">-->
+    <!--          <li class="flex items-center gap-1">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 1</span>-->
+    <!--          </li>-->
+    <!--          <li class="flex items-center gap-1 mt-2">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 2</span>-->
+    <!--          </li>-->
+    <!--          <li class="flex items-center gap-1 mt-2">-->
+    <!--            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-primary">-->
+    <!--              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"-->
+    <!--                    d="m19.75 7.018l-9.257 9.257a1 1 0 0 1-1.414 0L4.25 11.446"/>-->
+    <!--            </svg>-->
+    <!--            <span>Преимущество 3</span>-->
+    <!--          </li>-->
+    <!--        </ul>-->
+    <!--        <div class="mt-4 w-full">-->
+    <!--          <router-link to="/premium">-->
+    <!--            <button-->
+    <!--                class="bg-accent !text-secondary py-2 px-4 rounded-lg font-medium text-[20px] hover:bg-accent_hover w-full">-->
+    <!--              Купить-->
+    <!--            </button>-->
+    <!--          </router-link>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
   </div>
 </template>
 
